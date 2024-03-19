db488889eede902ef5d72236b214d53f524789f1
MurphyEdward <minexgleb@gmail.com>
2024-03-19 17:09:12 +0200

    add Swagger and pagination

--- src/main/java/com/github/onlinebookstore/services/BookService.java
+++ src/main/java/com/github/onlinebookstore/services/BookService.java
@@ -4,11 +4,12 @@ import com.github.onlinebookstore.dto.BookDto;
 import com.github.onlinebookstore.dto.BookSearchParameters;
 import com.github.onlinebookstore.dto.CreateBookRequestDto;
 import java.util.List;
+import org.springframework.data.domain.Pageable;
 
 public interface BookService {
     BookDto save(CreateBookRequestDto requestDto);
 
-    List<BookDto> findAll();
+    List<BookDto> findAll(Pageable pageable);
 
     BookDto findBookById(Long id);
 
--- src/main/java/com/github/onlinebookstore/services/impl/BookServiceImpl.java
+++ src/main/java/com/github/onlinebookstore/services/impl/BookServiceImpl.java
@@ -12,6 +12,7 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
@@ -29,8 +30,8 @@ public class BookServiceImpl implements BookService {
     }
 
     @Override
-    public List<BookDto> findAll() {
-        return bookRepository.findAll().stream()
+    public List<BookDto> findAll(Pageable pageable) {
+        return bookRepository.findAll(pageable).stream()
                 .map(bookMapper::toDto)
                 .toList();
     }
